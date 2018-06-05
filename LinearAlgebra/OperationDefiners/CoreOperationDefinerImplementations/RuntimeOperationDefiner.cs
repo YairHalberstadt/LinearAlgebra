d3b7b9ee3c0a852ecbf472ca9b7cf3801f063e61
yair halberstadt <yairhalberstadt@gmail.com>
2018-06-05 22:14:51 +0100

    began RuntimeImplementationDefiner implementation

---
+++ LinearAlgebra/OperationDefiners/CoreOperationDefinerImplementations/RuntimeOperationDefiner.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using OperationDefiners.CoreOperationDefinerInterfaces;
+
+namespace OperationDefiners.CoreOperationDefinerImplementations
+{
+    abstract class RuntimeOperationDefiner<T> : IOperationDefiner<T>
+    {
+    }
+
+    class RuntimeRingOperationDefiner<T> : RuntimeOperationDefiner<T>, IRingOperationDefiner<T>
+    {
+        public static RingOperationFuncs<T> Operations { get; }
+
+
+
+        public bool Equals(T first, T second)
+        {
+            throw new NotImplementedException();
+        }
+
+        public T Add(T first, T second)
+        {
+            throw new NotImplementedException();
+        }
+
+        public T Multiply(T first, T second)
+        {
+            throw new NotImplementedException();
+        }
+
+        public T Zero { get; }
+
+        public T Negative(T operand)
+        {
+            throw new NotImplementedException();
+        }
+
+        public T One { get; }
+    }
+
+    internal class RingOperationFuncs<T>
+    {
+    }
+}
