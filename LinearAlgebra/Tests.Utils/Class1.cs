8a4a24177feb51882220aead417f526c7843d237
yair halberstadt <yairhalberstadt@hotmail.co.uk>
2018-06-08 13:43:04 +0100

    Wrote Runtime Ring Operation definer Tests

---
+++ LinearAlgebra/Tests.Utils/Class1.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Tests.Utils
+{
+    public static class GetRandom
+    {
+	    public static IEnumerable<int> GetRandomInts(int numberOfInts =10000, int seed = 42, int maxAbsoluteSize= int.MaxValue)
+	    {
+		    var rand = new Random(42);
+		    for (int i = 0; i < numberOfInts; i++)
+			    yield return rand.Next(-maxAbsoluteSize, maxAbsoluteSize);
+	    }
+    }
+}
---
+++ LinearAlgebra/Tests.Utils/IVectorSetUpMethods.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using OperationDefiners.CoreOperationDefinerInterfaces;
+using Vectors;
+
+namespace Tests.Utils
+{
+    public static class IVectorSetUpMethods<TDataType, TOperationDefiner>
+	    where TOperationDefiner : IRingOperationDefiner<TDataType>, new()
+    {
+	    public static IEnumerable<IVector<TDataType, TOperationDefiner>> CreateRandomVectors(
+		    Func<IEnumerable<TDataType>, IVector<TDataType, TOperationDefiner>> createVectorFromIenumerable, Func<int,TDataType> createRandomTDataTypeFromInt, int numberOfVectors, int maxSize)
+	    {
+		    var rand = new Random(42);
+		    for (int i = 0; i < numberOfVectors; i++)
+		    {
+			    var size = rand.Next(0,maxSize);
+			    TDataType[] randomArray = new TDataType[size];
+			    for (int j = 0; j < size; j++)
+			    {
+				    randomArray[j] = createRandomTDataTypeFromInt(rand.Next());
+			    }
+
+			    yield return createVectorFromIenumerable(randomArray);
+		    }
+	    }
+    }
+}
